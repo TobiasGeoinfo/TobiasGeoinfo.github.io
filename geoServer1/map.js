c571ef70c24291fd062423bb25ecb9f2d4d6500c
TobiasGeoinfo <49439192+TobiasGeoinfo@users.noreply.github.com>
2019-11-14 16:24:33 +0100

    Add files via upload

---
+++ geoServer1/map.js
@@ -0,0 +1,24 @@
+var map = new ol.Map({
+        target: 'map',
+        layers: [
+		
+          new ol.layer.Tile({
+          source: new ol.source.TileWMS({
+			
+		
+		url: 'http://localhost:8080/geoserver/UebungGeodatendienste/wms',
+		params: {'LAYERS': 'UebungGeodatendienste:exports_percent_gdp', 'TILED': true},
+		serverType: 'geoserver'
+        
+								})
+      })
+
+        ],
+		
+		
+		
+        view: new ol.View({
+          center: ol.proj.fromLonLat([13, 51]),
+          zoom: 4
+        })
+      });
